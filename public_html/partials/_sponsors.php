8560f93770d3c437ec67fe1a2dcfee4af243c692
jpowleytxd <jorden.powley@zonal.co.uk>
2018-01-09 12:17:39 +0000

    Added new logos and adjusted header so that it doesn't overlay the nav.

--- public_html/partials/_sponsors.php
+++ public_html/partials/_sponsors.php
@@ -3,23 +3,23 @@
     <!-- Sponsors Section -->
     <div class="sponsors_outer">
         <div class="sponsor">
-            <img src="media/img/logos/Sky.jpg" alt="Sky">
+            <img src="media/img/logos/Sky.png" alt="Sky">
         </div>
         <div class="sponsor">
-            <img src="media/img/logos/Molson Coors.png" alt="Molson Coors">
-        </div>
-        <div class="clearfix"></div>
-
-        <div class="sponsor">
-            <img src="media/img/logos/Heineken Logo V2.jpg" alt="Heineken UK">
-        </div>
-        <div class="sponsor">
-            <img src="media/img/logos/CPL Training.png" alt="CPL Training">
+            <img src="media/img/logos/Molson-Coors.png" alt="Molson Coors">
         </div>
         <div class="clearfix"></div>
 
         <div class="sponsor">
-            <img src="media/img/logos/HIT Training Logo.png" alt="HIT Training">
+            <img src="media/img/logos/Heineken-Logo-V2.png" alt="Heineken UK">
+        </div>
+        <div class="sponsor">
+            <img src="media/img/logos/CPL-Training.png" alt="CPL Training">
+        </div>
+        <div class="clearfix"></div>
+
+        <div class="sponsor">
+            <img src="media/img/logos/HIT-Training-Logo.png" alt="HIT Training">
         </div>
         <div class="sponsor">
             <img src="media/img/logos/zonal-logo.png" alt="Zonal">
@@ -27,7 +27,7 @@
         <div class="clearfix"></div>
 
         <div class="sponsor">
-            <img src="media/img/logos/PURDEYS.jpg" alt="Purdeys">
+            <img src="media/img/logos/PURDEYS.png" alt="Purdeys">
         </div>
         <div class="sponsor ">
             <img class="blacken_logo" src="media/img/logos/MerchantsHeart_Logo.png" alt="Merchant Heart">
@@ -43,23 +43,18 @@
         <div class="clearfix"></div>
 
         <div class="sponsor">
-            <img src="media/img/logos/Jack Links Logo.jpg" alt="Jack Links">
+            <img src="media/img/logos/Jack-Links-Logo.png" alt="Jack Links">
         </div>
         <div class="sponsor">
-            <img src="media/img/logos/Red Bull Logo.png" alt="Red Bull">
+            <img src="media/img/logos/Red-Bull-Logo.png" alt="Red Bull">
         </div>
         <div class="clearfix"></div>
 
         <div class="sponsor">
-            <img src="media/img/logos/Jagermeister Logo.png" alt="Jagermeister">
+            <img src="media/img/logos/Jagermeister-Logo.png" alt="Jagermeister">
         </div>
         <div class="sponsor">
-            <img src="media/img/logos/Keel Toys Logo.jpg" alt="Keel Toys">
-        </div>
-        <div class="clearfix"></div>
-
-        <div class="sponsor" style="float: left;">
-            <img src="media/img/logos/Ribena Logo.png" alt="Ribena Sparkling">
+            <img src="media/img/logos/Keel-Toys-Logo.png" alt="Keel Toys">
         </div>
         <div class="clearfix"></div>
     </div>
